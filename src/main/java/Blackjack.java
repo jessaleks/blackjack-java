112b81646be543bbbfe959d364add10da361b4aa
Aleksander Jess <jess.aleksander@gmail.com>
2022-02-06 10:59:35 +0100

    implemented the required functionality, and all of the tests pass

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -1,10 +1,9 @@
-import com.sun.source.tree.DefaultCaseLabelTree;
-
-import java.util.List;
+import java.util.*;
 
 public class Blackjack {
-    final private List<Integer> values = List.of(11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 0);
+    // I would have used a Map here, though Java does not have a way to initialize a Map within a sane number of characters
     final private List<String> cards = List.of("ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "other");
+    final private List<Integer> values = List.of(11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 0);
 
     /**
      * calculate the score of any card at all
@@ -30,12 +29,44 @@ public class Blackjack {
         return parseCard(card1) + parseCard(card2) == 21;
     }
 
+    // no clue what to do here; I don't understand blackjack at all nor have I ever played it
+    // what does it mean to "have a figure"
+    // what does it mean to stand, hit or split?
+
+    /**
+     * Implements the logic when you have a hand with a score of 20 or more.
+     *
+     * @param isBlackjack is whether the hand is a blackjack
+     * @param dealerScore is the score of the dealer
+     */
     public String largeHand(boolean isBlackjack, int dealerScore) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.largeHand method");
+        if (isBlackjack) {
+            return dealerScore < 10 ? "W" : "S";
+        } else {
+            return "P";
+        }
+
     }
 
+    /**
+     * Implements the logic when you have a hand with a score of less than 20.
+     *
+     * @param handScore   is the score of the hand
+     * @param dealerScore is the score of the dealer
+     * @return the optimal decision for the hand
+     */
     public String smallHand(int handScore, int dealerScore) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
+        if (handScore >= 17) {
+            return "S";
+        }
+
+        if (handScore <= 11) {
+            return "H";
+        }
+
+        // Since after these two first checks, we know for sure our score
+        // is between 12 and 16, we only care about dealer's score now.
+        return dealerScore <= 6 ? "S" : "H";
     }
 
     // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
